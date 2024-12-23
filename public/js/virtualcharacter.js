aef54b26bb113434e3a1c22d4f993a6b9c89a377
rashi ghosh <89269151+rashighosh@users.noreply.github.com>
2024-12-23 11:00:04 -0700

    Update virtualcharacter.js

--- public/js/virtualcharacter.js
+++ public/js/virtualcharacter.js
@@ -4,6 +4,8 @@ var head; // TalkingHead instance
 
 const character = "/character-models/rashi.glb"
 
+var first=true;
+
 // Load and show the avatar
 document.addEventListener('DOMContentLoaded', async function (e) {
   const nodeAvatar = document.getElementById('virtualcharacter');
@@ -11,7 +13,7 @@ document.addEventListener('DOMContentLoaded', async function (e) {
     ttsEndpoint: "blah",
     lipsyncModules: ["en"], // language
     cameraView: "upper", // full, mid, upper, head
-    cameraDistance: -1, // negative is zoom in from base, postitive zoom out (in meters)
+    cameraDistance: -.5, // negative is zoom in from base, postitive zoom out (in meters)
     // interactions w 3d scene, usually disable
     cameraRotateEnable: false,
     cameraPanEnable: false,
@@ -44,37 +46,37 @@ document.addEventListener('DOMContentLoaded', async function (e) {
 
 });
 
-// // start audio for first agent audio (interrupts/disrupts any current audio)
-// export async function characterAudio(audio, emoji) {
-//   try {
-//     // if first audio turn, wave
-//     // chris: hand gestures are good, face gestures weird   
-//     if (first) {
-//       head.playGesture('👋');
-//       first = false;
-//     }
-//     else if (emoji) {
-//       head.playGesture(emoji);
-//     }
-//     head.replaceAndSpeakNewAudio(audio);
+// start audio for first agent audio (interrupts/disrupts any current audio)
+export async function characterAudio(audio, emoji) {
+  try {
+    // if first audio turn, wave
+    // chris: hand gestures are good, face gestures weird   
+    if (first) {
+      head.playGesture('👋');
+      first = false;
+    }
+    else if (emoji) {
+      head.playGesture(emoji);
+    }
+    head.replaceAndSpeakNewAudio(audio);
 
-//   } catch (error) {
-//     console.error('Error during speech processing:', error);
-//   }
-// }
+  } catch (error) {
+    console.error('Error during speech processing:', error);
+  }
+}
 
-// // for streaming audio, waits for current audio to finish
-// export async function characterAudioQueue(audio, emoji) {
-//   try {
-//     // console.log("Checking speaking: ", head.isSpeaking, head.speechQueue);      
-//     if (emoji) {
-//       head.playGesture(emoji);
-//     }
+// for streaming audio, waits for current audio to finish
+export async function characterAudioQueue(audio, emoji) {
+  // try {
+  //   // console.log("Checking speaking: ", head.isSpeaking, head.speechQueue);      
+  //   if (emoji) {
+  //     head.playGesture(emoji);
+  //   }
 
-//     // can have subtitles! and other stuff. hve to look more into if u want it
-//     head.speakAudio(audio, null, null);
+  //   // can have subtitles! and other stuff. hve to look more into if u want it
+  //   head.speakAudio(audio, null, null);
 
-//   } catch (error) {
-//     console.error('Error during speech processing:', error);
-//   }
-// }
+  // } catch (error) {
+  //   console.error('Error during speech processing:', error);
+  // }
+}
