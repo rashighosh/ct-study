06706ea243b26c0bef0728846e7244815e43f5e6
rghosh96 <rashighosh96@gmail.com>
2024-12-31 17:37:28 -0500

    desperate

--- server.js
+++ server.js
@@ -507,4 +507,8 @@ app.post('/updateTranscript', (req, res) => {
     });
   });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
